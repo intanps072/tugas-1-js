00b2a320c295432798ff7031f7adafcdeeba5ab8
intanps072 <ips.intan072@gmail.com>
2025-09-11 03:41:55 +0700

    Selesai

---
+++ nilai.js
@@ -0,0 +1,13 @@
+// Lulus pakai while //
+var nilai=6;
+
+while (nilai <=10) {
+    console.log("Nilai " + nilai + " = Lulus");
+    nilai++;
+}
+
+// Tidak Lulus pakai for //
+for (var nilai=1; nilai<6; nilai++){
+    console.log("Nilai " + nilai + " = Tidak Lulus")
+}
+     
