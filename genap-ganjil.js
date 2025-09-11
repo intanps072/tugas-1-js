e0ca821c5bde504b2d177e7a57b8908ee1948e46
intanps072 <ips.intan072@gmail.com>
2025-09-11 09:08:29 +0700

    Done

---
+++ genap-ganjil.js
@@ -0,0 +1,15 @@
+// Genap
+var angka1=0;
+    while (angka1 >= 0 && angka1 <= 14) {
+       console.log(angka1 + " = Genap");
+        angka1+=2;
+    }
+
+// Ganjil
+var angka2=1;
+    while (angka2 >= 1 && angka2 <= 15) {
+        console.log(angka2 + " = Ganjil");
+        angka2+=2;
+    }
+
+
